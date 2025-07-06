76f8741616d5c2a675c522cf419073f8b9cb2687
skanenje <swapomuse@gmail.com>
2025-07-06 15:22:54 +0300

    feat(view): implement customer profile view

--- netfix/views.py
+++ netfix/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from users.models import Customer
+from services.models import RequestedService
 
 from users.models import User, Company
 from services.models import Service
@@ -8,8 +10,20 @@ def home(request):
     return render(request, 'users/home.html', {'user': request.user})
 
 
-def customer_profile(request):
-    pass
+def customer_profile(request, name):
+    try:
+        user = User.objects.get(username=name)
+        customer = Customer.objects.get(user=user)
+    except (User.DoesNotExist, Customer.DoesNotExist):
+        # Handle the case where the user or customer doesn't exist
+        return render(request, 'users/profile.html', {'error': 'Customer not found'})
+
+    services = RequestedService.objects.filter(customer=customer)
+    return render(request, 'users/profile.html', {
+        'user': user,
+        'customer': customer,
+        'services': services
+    })
 
 
 def company_profile(request, name):
