f3190818cacd392ce25b37a245ed4afdfddc0982
skanenje <swapomuse@gmail.com>
2025-07-10 18:16:48 +0300

    fix: registration page layout

--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,6 @@ from . import views as v
 
 urlpatterns = [
     path('', v.register, name='register'),
-    path('company/', v.CompanySignUpView.as_view(), name='register_company'),
-    path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
-    path('login/', v.LoginUserView, name='login_user'),
-    path('profile/', v.profile_view, name='profile')
+        path('login/', v.LoginUserView, name='login_user'),
+        path('profile/', v.profile_view, name='profile')
 ]
--- users/views.py
+++ users/views.py
@@ -11,44 +11,13 @@ from services.models import RequestedService
 def register(request):
     return render(request, 'users/register.html')
 
-
-class CustomerSignUpView(CreateView):
-    model = User
-    form_class = CustomerSignUpForm
-    template_name = 'users/register_customer.html'
-
-    def get_context_data(self, **kwargs):
-        kwargs['user_type'] = 'customer'
-        return super().get_context_data(**kwargs)
-
-    def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)
-        return redirect('/')
-
-
-class CompanySignUpView(CreateView):
-    model = User
-    form_class = CompanySignUpForm
-    template_name = 'users/register_company.html'
-
-    def get_context_data(self, **kwargs):
-        kwargs['user_type'] = 'company'
-        return super().get_context_data(**kwargs)
-
-    def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)
-        return redirect('/')
-
-
 def LoginUserView(request):
     if request.method == 'POST':
         form = UserLoginForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            user = authenticate(email=email, password=password)
+            user = authenticate(request, email=email, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('/')
@@ -56,7 +25,6 @@ def LoginUserView(request):
         form = UserLoginForm()
     return render(request, 'users/login.html', {'form': form})
 
-@login_required
 def profile_view(request):
     user = request.user
     if user.is_customer:
