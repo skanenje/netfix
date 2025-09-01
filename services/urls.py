d5069d01d002dc8b48a1da2355ede04c3262c75c
skanenje <swapomuse@gmail.com>
2025-09-01 17:32:54 +0300

    fix missing feilds

--- services/urls.py
+++ services/urls.py
@@ -6,8 +6,8 @@ app_name = 'services'
 urlpatterns = [
     path('', views.service_list, name='services_list'),
     path('create/', views.create_service, name='services_create'),
+    path('most-requested/', views.most_requested, name='most_requested'),
     path('<int:pk>/', views.service_detail, name='service_detail'),
     path('<int:pk>/request_service/', views.request_service, name='request_service'),
     path('<slug:field>/', views.service_field, name='services_field'),
-    path('most-requested/', views.most_requested, name='most_requested'),
 ]
